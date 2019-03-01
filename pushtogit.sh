31d86ee1eccf3b5f742f5935b1a926fe98328ff7
nvdinh185 <nvdinh0905671805@gmail.com>
2019-03-01 16:07:56 +0700

    first commit

--- pushtogit.sh
+++ pushtogit.sh
@@ -1,5 +1,5 @@
-git init
+#git init
 git add .
 git commit -m "first commit"
-git remote add origin https://github.com/nvdinh185/async-await.git
+#git remote add origin https://github.com/nvdinh185/async-await.git
 git push -u origin master
