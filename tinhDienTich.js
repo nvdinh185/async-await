420d25875edff9adbeb7222e2cc3953adb3e71e4
nvdinh09thxd <nvdinh09thxd@gmail.com>
2020-09-07 09:47:08 +0700

    edit promise

--- tinhDienTich.js
+++ tinhDienTich.js
@@ -1,38 +1,32 @@
-let add = (a, b) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (typeof (a) != "number" || typeof (b) != "number") {
-                return reject(new Error("Tham so phai co kieu number!"));
-            }
-            resolve(a + b);
-        }, 1000)
-    })
-}
+let add = (a, b) => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (typeof (a) != "number" || typeof (b) != "number") {
+            return reject(new Error("Tham so phai co kieu number!"));
+        }
+        resolve(a + b);
+    }, 1000)
+})
 
-let multiply = (a, b) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (typeof (a) != "number" || typeof (b) != "number") {
-                return reject(new Error("Tham so phai co kieu number!"));
-            }
-            resolve(a * b);
-        }, 1000)
-    })
-}
+let multiply = (a, b) => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (typeof (a) != "number" || typeof (b) != "number") {
+            return reject(new Error("Tham so phai co kieu number!"));
+        }
+        resolve(a * b);
+    }, 1000)
+})
 
-let devide = (a, b) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (typeof (a) != "number" || typeof (b) != "number") {
-                return reject(new Error("Tham so phai co kieu number!"));
-            }
-            if (b == 0) {
-                return reject(new Error("Khong the chia cho 0!"));
-            }
-            resolve(a / b);
-        }, 1000)
-    })
-}
+let devide = (a, b) => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (typeof (a) != "number" || typeof (b) != "number") {
+            return reject(new Error("Tham so phai co kieu number!"));
+        }
+        if (b == 0) {
+            return reject(new Error("Khong the chia cho 0!"));
+        }
+        resolve(a / b);
+    }, 1000)
+})
 
 let tinhDienTich = async (a, b, h, cb) => {
     try {
--- tinhDienTichPr.js
+++ tinhDienTichPr.js
@@ -1,40 +1,34 @@
-let add = (a, b) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (typeof (a) != "number" || typeof (b) != "number") {
-                return reject(new Error("Tham so phai co kieu number!"));
-            }
-            resolve(a + b);
-        }, 1000)
-    })
-}
+let add = (a, b) => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (typeof (a) != "number" || typeof (b) != "number") {
+            return reject(new Error("Tham so phai co kieu number!"));
+        }
+        resolve(a + b);
+    }, 1000)
+})
 
-let multiply = (a, b) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (typeof (a) != "number" || typeof (b) != "number") {
-                return reject(new Error("Tham so phai co kieu number!"));
-            }
-            resolve(a * b);
-        }, 1000)
-    })
-}
+let multiply = (a, b) => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (typeof (a) != "number" || typeof (b) != "number") {
+            return reject(new Error("Tham so phai co kieu number!"));
+        }
+        resolve(a * b);
+    }, 1000)
+})
 
-let devide = (a, b) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (typeof (a) != "number" || typeof (b) != "number") {
-                return reject(new Error("Tham so phai co kieu number!"));
-            }
-            if (b == 0) {
-                return reject(new Error("Khong the chia cho 0!"));
-            }
-            resolve(a / b);
-        }, 1000)
-    })
-}
+let devide = (a, b) => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (typeof (a) != "number" || typeof (b) != "number") {
+            return reject(new Error("Tham so phai co kieu number!"));
+        }
+        if (b == 0) {
+            return reject(new Error("Khong the chia cho 0!"));
+        }
+        resolve(a / b);
+    }, 1000)
+})
 
-let tinhDienTich = async (a, b, h) => {
+let tinhDienTich1 = async (a, b, h) => {
     try {
         let ab = await add(a, b);
         let abh = await multiply(ab, h);
@@ -44,7 +38,24 @@ let tinhDienTich = async (a, b, h) => {
         return Promise.reject(e);
     }
 }
+let tinhDienTich2 = (a, b, h) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let ab = await add(a, b);
+            let abh = await multiply(ab, h);
+            let square = await devide(abh, 2);
+            resolve(square);
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
 
-tinhDienTich(4, 5, 6)
-    .then(res => console.log(res))
-    .catch(err => console.log(err + ''))
+(async () => {
+    try {
+        let square = await tinhDienTich2(4, 5, 6);
+        console.log("Dien tich: ", square);
+    } catch (err) {
+        console.log(err + '');
+    }
+})();
